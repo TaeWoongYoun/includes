768abc49314396e2f64f811be0400e55fc26261c
xodnd0507 <24457545yong@gmail.com>
2024-02-19 12:10:58 +0900

    feat : Login page design

---
+++ index.js
@@ -0,0 +1,20 @@
+$('#login').on('click', function(){
+    $('.black-bg').addClass('showModal');
+});
+
+$('#close').on('click', function(){
+    $('.black-bg').removeClass('showModal');
+});
+
+$('#send').on('click', function(e){
+    if (document.getElementById('email').value == ''){
+        alert('아이디를 입력해주세요.');
+        e.preventDefault();
+    } else if (document.getElementById('password').value == ''){
+        alert('비밀번호를 입력해주세요.');
+        e.preventDefault();
+    } else if(document.getElementById('password').value.length < 6){
+        alert('비밀번호를 6자 이상 입력해주세요');
+        e.preventDefault();
+    }
+});
