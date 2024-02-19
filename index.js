fc1f3075e5b1389c9550bfb9853e3c6443c2f6e7
xodnd0507 <24457545yong@gmail.com>
2024-02-19 14:12:05 +0900

    Test : false

--- index.js
+++ index.js
@@ -7,14 +7,25 @@ $('#close').on('click', function(){
 });
 
 $('#send').on('click', function(e){
-    if (document.getElementById('email').value == ''){
+    var 아이디 = document.getElementById('email').value;
+    var 비밀번호 = document.getElementById('password').value;
+
+    if (아이디 == ''){
         alert('아이디를 입력해주세요.');
         e.preventDefault();
-    } else if (document.getElementById('password').value == ''){
+    } else if (비밀번호 == ''){
         alert('비밀번호를 입력해주세요.');
         e.preventDefault();
     } else if(document.getElementById('password').value.length < 6){
         alert('비밀번호를 6자 이상 입력해주세요');
+        e.preventDefault();
+    }
+
+    if(/\S+@\S+.\S+/.test(아이디)){
+        alert('이메일 형식이 아닙니다.')
+        e.preventDefault();
+    }else if(/[A-Z]/.test(비밀번호 == false)){
+        alert('비밀번호에 대문자를 추가해주세요.')
         e.preventDefault();
     }
 });
